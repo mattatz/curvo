443e0f0c020550fac252cc483a834cb3f7e0dd37
mattatz <masatatsu.nakamura@gmail.com>
2024-05-29 16:48:55 +0900

    Randomize division point in BoundingBoxTree.

--- src/bounding_box/bounding_box_tree.rs
+++ src/bounding_box/bounding_box_tree.rs
@@ -1,6 +1,7 @@
 use std::borrow::Cow;
 
 use nalgebra::{allocator::Allocator, DefaultAllocator, DimName, DimNameDiff, DimNameSub, U1};
+use rand::Rng;
 
 use crate::{bounding_box::BoundingBox, curve::nurbs_curve::NurbsCurve, misc::FloatingPoint};
 
@@ -25,6 +26,7 @@ where
         let tol = tolerance.unwrap_or_else(|| {
             let i = curve.knots_domain_interval();
             i / T::from_usize(64).unwrap()
+            // i / T::from_usize(128).unwrap()
         });
         BoundingBoxTree {
             curve: Cow::Borrowed(curve),
@@ -48,11 +50,16 @@ where
 
     /// Try to divide the curve into two parts.
     pub fn try_divide(&self) -> anyhow::Result<(Self, Self)> {
+        // let min = self.curve.knots().first();
+        // let max = self.curve.knots().last();
         let (min, max) = self.curve.knots_domain();
         let interval = max - min;
-        let (head, tail) = self
-            .curve
-            .try_trim(min + interval / T::from_usize(2).unwrap())?;
+        let mid = (min + max) / T::from_usize(2).unwrap();
+
+        let mut rng = rand::thread_rng();
+        let r = interval * T::from_f64(1e-1 * rng.gen::<f64>()).unwrap();
+
+        let (head, tail) = self.curve.try_trim(mid + r)?;
         Ok((
             Self {
                 curve: Cow::Owned(head),
