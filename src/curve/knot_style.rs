f06275fe0a1cdb48101e8647a4248eb4584c08f1
mattatz <masatatsu.nakamura@gmail.com>
2024-07-04 10:59:25 +0900

    Create KnotStyle.

---
+++ src/curve/knot_style.rs
@@ -0,0 +1,54 @@
+use itertools::Itertools;
+use nalgebra::{allocator::Allocator, DVector, DefaultAllocator, DimName, OPoint};
+
+use crate::misc::FloatingPoint;
+
+#[derive(Debug, Clone)]
+pub enum KnotStyle {
+    Uniform,
+    Chordal,
+    Centripetal,
+}
+
+impl KnotStyle {
+    pub fn parameterize<T: FloatingPoint>(&self, points: &[DVector<T>], closed: bool) -> Vec<T> where
+    {
+        match self {
+            KnotStyle::Uniform => {
+                let n = if closed {
+                    points.len()
+                } else {
+                    points.len() - 1
+                };
+                let inv = T::one() / T::from_usize(n - 1).unwrap();
+                (0..n).map(|_| inv).collect()
+            }
+            KnotStyle::Chordal | KnotStyle::Centripetal => {
+                let alpha = self.alpha();
+
+                let params: Vec<T> = if closed {
+                    points
+                        .iter()
+                        .circular_tuple_windows()
+                        .map(|(a, b)| (b - a).norm().powf(alpha))
+                        .collect()
+                } else {
+                    points
+                        .iter()
+                        .tuple_windows()
+                        .map(|(a, b)| (b - a).norm().powf(alpha))
+                        .collect()
+                };
+
+                params
+            }
+        }
+    }
+
+    pub fn alpha<T: FloatingPoint>(&self) -> T {
+        match self {
+            KnotStyle::Uniform | KnotStyle::Chordal => T::one(),
+            KnotStyle::Centripetal => T::from_f64(0.5).unwrap(),
+        }
+    }
+}
--- src/curve/mod.rs
+++ src/curve/mod.rs
@@ -1,4 +1,6 @@
 pub mod curve_length_parameter;
+pub mod knot_style;
 pub mod nurbs_curve;
 pub use curve_length_parameter::*;
+pub use knot_style::*;
 pub use nurbs_curve::*;
