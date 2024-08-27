2ca1098f7ac4566147859ae21b6159eaaad06aea
mattatz <masatatsu.nakamura@gmail.com>
2024-08-27 17:33:44 +0900

    Enable to test orientation & line intersection robustly.

---
+++ src/misc/line.rs
@@ -0,0 +1,74 @@
+use nalgebra::{Const, Point2};
+use num_traits::NumCast;
+use robust::{orient2d, Coord};
+
+use crate::prelude::BoundingBox;
+
+use super::{orientation, FloatingPoint, Orientation};
+
+/// A struct representing a line in 2D space.
+pub struct Line<T: FloatingPoint> {
+    start: Point2<T>,
+    end: Point2<T>,
+}
+
+impl<T: FloatingPoint> Line<T> {
+    pub fn new(start: Point2<T>, end: Point2<T>) -> Self {
+        Self { start, end }
+    }
+
+    pub fn start(&self) -> &Point2<T> {
+        &self.start
+    }
+
+    pub fn end(&self) -> &Point2<T> {
+        &self.end
+    }
+
+    /// Robust intersection test between two lines.
+    pub fn intersects(&self, other: &Line<T>) -> bool {
+        if !self
+            .bounding_rect()
+            .intersects(&other.bounding_rect(), None)
+        {
+            return false;
+        }
+
+        let p_q1 = orientation(self.start(), self.end(), other.start());
+        let p_q2 = orientation(self.start(), self.end(), other.end());
+
+        if matches!(
+            (p_q1, p_q2),
+            (Orientation::Clockwise, Orientation::Clockwise)
+                | (Orientation::CounterClockwise, Orientation::CounterClockwise)
+        ) {
+            return false;
+        }
+
+        let q_p1 = orientation(other.start(), other.end(), self.start());
+        let q_p2 = orientation(other.start(), other.end(), self.end());
+        if matches!(
+            (q_p1, q_p2),
+            (Orientation::Clockwise, Orientation::Clockwise)
+                | (Orientation::CounterClockwise, Orientation::CounterClockwise)
+        ) {
+            return false;
+        }
+
+        if matches!(
+            (p_q1, p_q2, q_p1, q_p2),
+            (Orientation::Collinear, Orientation::Collinear, Orientation::Collinear, Orientation::Collinear)
+        ) {
+            return false;
+        }
+
+        // println!("{:?} {:?} {:?} {:?}", p_q1, p_q2, q_p1, q_p2);
+
+        true
+    }
+
+    /// Returns the bounding box of the line.
+    pub fn bounding_rect(&self) -> BoundingBox<T, Const<2>> {
+        BoundingBox::new(self.start.coords, self.end.coords)
+    }
+}
