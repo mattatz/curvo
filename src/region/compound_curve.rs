fb2b01295d6b564577ad278c03dbec0ce26dbe61
mattatz <masatatsu.nakamura@gmail.com>
2024-09-02 21:31:25 +0900

    WIP.

--- src/region/compound_curve.rs
+++ src/region/compound_curve.rs
@@ -1,7 +1,11 @@
 use nalgebra::{allocator::Allocator, DefaultAllocator, DimName, DimNameDiff, DimNameSub, U1};
 
-use crate::{curve::NurbsCurve, misc::FloatingPoint};
+use crate::{
+    curve::NurbsCurve,
+    misc::{FloatingPoint, Invertible},
+};
 
+/// A struct representing a compound curve.
 #[derive(Clone, Debug)]
 pub struct CompoundCurve<T: FloatingPoint, D: DimName>
 where
@@ -14,7 +18,34 @@ impl<T: FloatingPoint, D: DimName> CompoundCurve<T, D>
 where
     DefaultAllocator: Allocator<D>,
 {
-    pub fn new(spans: Vec<NurbsCurve<T, D>>) -> Self {
+    pub fn new(mut spans: Vec<NurbsCurve<T, D>>) -> Self
+    where
+        D: DimNameSub<U1>,
+        DefaultAllocator: Allocator<DimNameDiff<D, U1>>,
+    {
+        let eps = T::from_f64(1e-5).unwrap();
+
+        // Ensure the adjacent spans are connected in the forward direction.
+        let n = spans.len();
+        for i in 0..(n - 1) {
+            let a = &spans[i];
+            let b = &spans[i + 1];
+            let direction = Direction::new(a, b, eps);
+            match direction {
+                Direction::Forward => {}
+                Direction::Backward => {
+                    spans[i].invert();
+                    spans[i + 1].invert();
+                }
+                Direction::Facing => {
+                    spans[i + 1].invert();
+                }
+                Direction::Opposite => {
+                    spans[i].invert();
+                }
+            }
+        }
+
         Self { spans }
     }
 
@@ -53,8 +84,50 @@ where
 impl<T: FloatingPoint, D: DimName> From<NurbsCurve<T, D>> for CompoundCurve<T, D>
 where
     DefaultAllocator: Allocator<D>,
+    D: DimNameSub<U1>,
+    DefaultAllocator: Allocator<DimNameDiff<D, U1>>,
 {
     fn from(value: NurbsCurve<T, D>) -> Self {
         Self::new(vec![value])
+    }
+}
+
+/// Direction of the two connected curves.
+#[derive(Clone, Debug)]
+enum Direction {
+    Forward,  // -> ->
+    Backward, // <- <-
+    Facing,   // -> <-
+    Opposite, // <- ->
+}
+
+impl Direction {
+    fn new<T: FloatingPoint, D: DimName>(
+        a: &NurbsCurve<T, D>,
+        b: &NurbsCurve<T, D>,
+        epsilon: T,
+    ) -> Self
+    where
+        DefaultAllocator: Allocator<D>,
+        D: DimNameSub<U1>,
+        DefaultAllocator: Allocator<DimNameDiff<D, U1>>,
+    {
+        let ad = a.knots_domain();
+        let bd = b.knots_domain();
+        let (a0, a1) = (a.point_at(ad.0), a.point_at(ad.1));
+        let (b0, b1) = (b.point_at(bd.0), b.point_at(bd.1));
+        let d10 = &a1 - &b0;
+        if d10.norm() < epsilon {
+            return Self::Forward;
+        }
+        let d01 = &a0 - &b1;
+        if d01.norm() < epsilon {
+            return Self::Backward;
+        }
+        let d11 = &a1 - &b1;
+        if d11.norm() < epsilon {
+            return Self::Facing;
+        }
+        Self::Opposite
     }
 }
