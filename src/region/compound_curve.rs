38af131c80300661df4c13924bcf42e0ba9fe417
mattatz <masatatsu.nakamura@gmail.com>
2024-09-06 13:17:10 +0900

    Align knot vectors for spans in CompundCurve & impl Conpound::point_at.

--- src/region/compound_curve.rs
+++ src/region/compound_curve.rs
@@ -32,6 +32,8 @@ impl<T: FloatingPoint, D: DimName> CompoundCurve<T, D>
 where
     DefaultAllocator: Allocator<D>,
 {
+    /// Create a new compound curve from a list of spans.
+    /// The spans must be connected.
     pub fn new(spans: Vec<NurbsCurve<T, D>>) -> Self
     where
         D: DimNameSub<U1>,
@@ -77,6 +79,17 @@ where
             }
         }
 
+        // Align knot vectors
+        // The first knot vector starts at 0, the rest are aligned to the previous knot vector
+        let mut knot_offset = T::zero();
+        connected.iter_mut().for_each(|curve| {
+            let start = curve.knots().first();
+            curve.knots_mut().iter_mut().for_each(|v| {
+                *v = *v - start + knot_offset;
+            });
+            knot_offset = curve.knots().last();
+        });
+
         Self { spans: connected }
     }
 
@@ -86,6 +99,37 @@ where
 
     pub fn spans_mut(&mut self) -> &mut [NurbsCurve<T, D>] {
         &mut self.spans
+    }
+
+    /// Evaluate the curve containing the parameter t at the given parameter t.
+    /// ```
+    /// use curvo::prelude::*;
+    /// use nalgebra::{Point2, Vector2};
+    /// use std::f64::consts::{FRAC_PI_2, PI, TAU};
+    /// use approx::assert_relative_eq;
+    /// let o = Point2::origin();
+    /// let dx = Vector2::x();
+    /// let dy = Vector2::y();
+    /// let compound = CompoundCurve::new(vec![
+    ///     NurbsCurve2D::try_arc(&o, &dx, &dy, 1., 0., PI).unwrap(),
+    ///     NurbsCurve2D::try_arc(&o, &dx, &dy, 1., PI, TAU).unwrap(),
+    /// ]);
+    /// assert_relative_eq!(compound.point_at(0.).unwrap(), Point2::new(1., 0.), epsilon = 1e-5);
+    /// assert_relative_eq!(compound.point_at(FRAC_PI_2).unwrap(), Point2::new(0., 1.), epsilon = 1e-5);
+    /// assert_relative_eq!(compound.point_at(PI).unwrap(), Point2::new(-1., 0.), epsilon = 1e-5);
+    /// assert_relative_eq!(compound.point_at(PI + FRAC_PI_2).unwrap(), Point2::new(0., -1.), epsilon = 1e-5);
+    /// assert_relative_eq!(compound.point_at(TAU).unwrap(), Point2::new(1., 0.), epsilon = 1e-5);
+    /// ```
+    pub fn point_at(&self, t: T) -> Option<OPoint<T, DimNameDiff<D, U1>>>
+    where
+        D: DimNameSub<U1>,
+        DefaultAllocator: Allocator<DimNameDiff<D, U1>>,
+    {
+        let span = self.spans.iter().find(|span| {
+            let (d0, d1) = span.knots_domain();
+            (d0..=d1).contains(&t)
+        });
+        span.map(|span| span.point_at(t))
     }
 
     /// Returns the total length of the compound curve.
@@ -188,13 +232,6 @@ where
 
         let mut res = res?;
         let eps = T::from_f64(1e-2).unwrap();
-
-        /*
-        res.iter().for_each(|it| {
-            let pts = it.iter().map(|i| &i.a().1).collect_vec();
-            println!("{:?}", pts);
-        });
-        */
 
         (0..res.len()).circular_tuple_windows().for_each(|(a, b)| {
             if a != b {
