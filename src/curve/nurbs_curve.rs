e8991ceb841c9d8e4a8304984c0c878da1ebbc1d
mattatz <masatatsu.nakamura@gmail.com>
2024-10-22 11:05:49 +0900

    Return zero if segments is empty in try_length.

--- src/curve/nurbs_curve.rs
+++ src/curve/nurbs_curve.rs
@@ -459,10 +459,9 @@ where
         };
         let segments = &segments[i..j];
 
-        anyhow::ensure!(
-            !segments.is_empty(),
-            "Decomposed segments are empty to compute the length"
-        );
+        if segments.is_empty() {
+            return Ok(T::zero());
+        }
 
         let (_, u) = self.knots_domain();
         let gauss = GaussLegendre::new(16 + self.degree)?;
