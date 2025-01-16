368e325f1dbf8f9be61d06e4a16cedabdf86004b
mattatz <masatatsu.nakamura@gmail.com>
2025-01-16 21:25:19 +0900

    Create constrained trimmed surface tessellation example.

--- src/tessellation/trimmed_surface_constraints.rs
+++ src/tessellation/trimmed_surface_constraints.rs
@@ -1,7 +1,7 @@
 use crate::misc::FloatingPoint;
 
 /// A struct representing constraints for a trimmed surface tessellation
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct TrimmedSurfaceConstraints<T: FloatingPoint> {
     exterior: Option<Vec<T>>,
     interiors: Vec<Option<Vec<T>>>,
@@ -13,6 +13,20 @@ impl<T: FloatingPoint> TrimmedSurfaceConstraints<T> {
             exterior,
             interiors,
         }
+    }
+
+    pub fn with_exterior(mut self, exterior: Option<Vec<T>>) -> Self {
+        self.exterior = exterior;
+        self
+    }
+
+    pub fn with_interiors(mut self, interiors: Vec<Option<Vec<T>>>) -> Self {
+        self.interiors = interiors;
+        self
+    }
+
+    pub fn push_interior(&mut self, interior: Option<Vec<T>>) {
+        self.interiors.push(interior);
     }
 
     pub fn exterior(&self) -> Option<&Vec<T>> {
