3f8e13e05e18a9c1da9f6d1edb4bfcfe027e85bc
mattatz <masatatsu.nakamura@gmail.com>
2025-06-13 16:42:34 +0900

    WIP.

--- examples/offset_curve.rs
+++ examples/offset_curve.rs
@@ -1,8 +1,9 @@
 use bevy::{
-    color::palettes::css::{TOMATO, WHITE},
+    color::palettes::css::{TOMATO, WHITE, YELLOW},
     prelude::*,
     render::camera::ScalingMode,
 };
+use bevy_egui::{egui, EguiContextPass, EguiContexts, EguiPlugin};
 use bevy_infinite_grid::InfiniteGridPlugin;
 
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
@@ -11,6 +12,7 @@ use bevy_points::{
     mesh::PointsMesh,
     plugin::PointsPlugin,
 };
+use itertools::Itertools;
 use misc::add_curve;
 use nalgebra::Point2;
 
@@ -20,6 +22,40 @@ mod materials;
 mod misc;
 
 use materials::*;
+use misc::*;
+
+#[derive(Resource)]
+struct Setting {
+    pub min_distance: f64,
+    pub max_distance: f64,
+    pub distance: f64,
+}
+
+impl Default for Setting {
+    fn default() -> Self {
+        Self {
+            min_distance: -10.0,
+            max_distance: 10.0,
+            // distance: 0.1,
+            distance: -0.1,
+        }
+    }
+}
+
+#[derive(Component)]
+struct ProfileCurve(pub NurbsCurve2D<f64>);
+
+#[derive(Component)]
+struct OffsetVertex(pub CompoundCurve2D<f64>);
+
+#[derive(Component)]
+struct OffsetCurve(pub CompoundCurve2D<f64>);
+
+impl OffsetCurve {
+    pub fn update(&mut self, entity: CompoundCurve2D<f64>) {
+        self.0 = entity;
+    }
+}
 
 fn main() {
     App::new()
@@ -28,6 +64,9 @@ fn main() {
         .add_plugins(InfiniteGridPlugin)
         .add_plugins(PanOrbitCameraPlugin)
         .add_plugins(PointsPlugin)
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
         .add_plugins(AppPlugin)
         .run();
 }
@@ -35,7 +74,17 @@ struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(Startup, setup);
+        app.insert_resource(Setting::default())
+            .add_systems(Startup, setup)
+            .add_systems(
+                PreUpdate,
+                (absorb_egui_inputs,)
+                    .after(bevy_egui::input::write_egui_input_system)
+                    .before(bevy_egui::begin_pass_system),
+            )
+            .add_systems(EguiContextPass, update_ui)
+            .add_systems(Update, gizmos_offset_curve)
+        ;
     }
 }
 
@@ -44,15 +93,17 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut line_materials: ResMut<Assets<LineMaterial>>,
     mut points_materials: ResMut<Assets<PointsMaterial>>,
+    settings: Res<Setting>,
 ) {
     let points = vec![
         Point2::new(-1.0, -1.0),
         Point2::new(1.0, -1.0),
-        Point2::new(1.0, 0.0),
-        Point2::new(-1.0, 0.0),
-        Point2::new(-1.0, 1.0),
         Point2::new(1.0, 1.0),
+        Point2::new(-1.0, 1.0),
+        Point2::new(-1.0, 3.0),
+        Point2::new(1.0, 3.0),
     ];
+
     commands
         .spawn((
             Mesh3d(
@@ -76,16 +127,25 @@ fn setup(
         ))
         .insert(Name::new("points"));
 
-    let polyline_curve = NurbsCurve2D::polyline(&points, true);
-
+    let curve = NurbsCurve2D::polyline(&points, true);
+    // let curve = NurbsCurve2D::try_interpolate(&points, 2).unwrap();
     add_curve(
-        &polyline_curve.elevate_dimension(),
+        &curve.elevate_dimension(),
         Some(WHITE.into()),
         Some(1e-8),
         &mut commands,
         &mut meshes,
         &mut line_materials,
     );
+
+    let option = CurveOffsetOption::default()
+        .with_distance(settings.distance)
+        .with_normal_tolerance(1e-4);
+    let offset_curve = curve.offset(option).unwrap();
+
+    commands.spawn((ProfileCurve(curve),));
+    commands.spawn((OffsetCurve(offset_curve.clone()),));
+    commands.spawn((OffsetVertex(offset_curve),));
 
     let scale = 5.;
     commands.spawn((
@@ -101,4 +161,60 @@ fn setup(
         Transform::from_translation(Vec3::new(0., 0., 3.)).looking_at(Vec3::ZERO, Vec3::Y),
         PanOrbitCamera::default(),
     ));
+}
+
+fn update_ui(
+    mut contexts: EguiContexts,
+    mut settings: ResMut<Setting>,
+    profile: Query<&ProfileCurve>,
+    mut offset_curve: Query<&mut OffsetCurve>,
+    mut offset_vertex: Query<&mut OffsetVertex>,
+) {
+    egui::Window::new("offset curve")
+        .collapsible(false)
+        .drag_to_scroll(false)
+        .default_width(420.)
+        .min_width(420.)
+        .max_width(420.)
+        .show(contexts.ctx_mut(), |ui| {
+            let response = ui.add(
+                egui::DragValue::new(&mut settings.distance).speed(1e-2)
+            );
+            if response.changed() {
+                let profile = profile.single().unwrap();
+                let mut offset_curve = offset_curve.single_mut().unwrap();
+                let option = CurveOffsetOption::default()
+                    .with_distance(settings.distance)
+                    .with_normal_tolerance(1e-4);
+                let res = profile.0.offset(option);
+                if let Ok(res) = res {
+                    offset_curve.update(res);
+                }
+
+                let option = CurveOffsetOption::default()
+                    .with_corner_type(CurveOffsetCornerType::None)
+                    .with_distance(settings.distance)
+                    .with_normal_tolerance(1e-4);
+                let res = profile.0.offset(option);
+                if let Ok(res) = res {
+                    offset_vertex.single_mut().unwrap().0 = res;
+                }
+            }
+        });
+}
+
+fn gizmos_offset_curve(offset_curve: Query<&OffsetCurve>, offset_vertex: Query<&OffsetVertex>, mut gizmos: Gizmos) {
+    let offset_curve = offset_curve.single().unwrap();
+    offset_curve.0.spans().iter().for_each(|span| {
+        let c = span.elevate_dimension();
+        let tess = c.tessellate(None).into_iter().map(|p| p.cast::<f32>().into()).collect_vec();
+        gizmos.linestrip(tess, WHITE);
+    });
+
+    let offset_vertex = offset_vertex.single().unwrap();
+    offset_vertex.0.spans().iter().for_each(|span| {
+        let c = span.elevate_dimension();
+        let tess = c.tessellate(None).into_iter().map(|p| p.cast::<f32>().into()).collect_vec();
+        gizmos.linestrip(tess, YELLOW);
+    });
 }
---
+++ src/offset/offset_nurbs_curve.rs
@@ -0,0 +1,365 @@
+use geo::LineIntersection;
+use itertools::Itertools;
+use nalgebra::allocator::Allocator;
+use nalgebra::{
+    DefaultAllocator, DimName, DimNameDiff, DimNameSub, OPoint, OVector, Point, Point2, Vector2, U1,
+};
+use num_traits::NumCast;
+
+use crate::curve::{NurbsCurve2D, NurbsCurve3D};
+use crate::offset::CurveOffsetCornerType;
+use crate::region::CompoundCurve2D;
+use crate::tessellation::tessellation_curve::tessellate_curve_adaptive;
+use crate::{curve::NurbsCurve, misc::FloatingPoint, offset::Offset};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CurveOffsetOption<T> {
+    distance: T,
+    normal_tolerance: T,
+    corner_type: CurveOffsetCornerType,
+}
+
+impl<T: FloatingPoint> Default for CurveOffsetOption<T> {
+    fn default() -> Self {
+        Self {
+            distance: T::zero(),
+            normal_tolerance: T::from_f64(1e-4).unwrap(),
+            corner_type: Default::default(),
+        }
+    }
+}
+
+impl<T> CurveOffsetOption<T> {
+    pub fn with_distance(mut self, distance: T) -> Self {
+        self.distance = distance;
+        self
+    }
+
+    pub fn with_normal_tolerance(mut self, tol: T) -> Self {
+        self.normal_tolerance = tol;
+        self
+    }
+
+    pub fn with_corner_type(mut self, ty: CurveOffsetCornerType) -> Self {
+        self.corner_type = ty;
+        self
+    }
+}
+
+/// vertex variant
+#[derive(Debug, Clone, PartialEq)]
+enum Vertex<T: FloatingPoint> {
+    Point(Point2<T>),
+    Intersection(Point2<T>),
+}
+
+impl<T: FloatingPoint, P: geo::CoordNum> From<geo::Point<P>> for Vertex<T> {
+    fn from(p: geo::Point<P>) -> Self {
+        let x = p.x().to_f64().unwrap();
+        let y = p.y().to_f64().unwrap();
+        Vertex::Point(Point2::new(
+            T::from_f64(x).unwrap(),
+            T::from_f64(y).unwrap(),
+        ))
+    }
+}
+
+impl<T: FloatingPoint> From<Vertex<T>> for Point2<T> {
+    fn from(v: Vertex<T>) -> Self {
+        match v {
+            Vertex::Point(p) => p,
+            Vertex::Intersection(p) => p,
+        }
+    }
+}
+
+impl<T: FloatingPoint> Vertex<T> {
+    pub fn point<P: geo::CoordNum>(p: geo::Point<P>) -> Self {
+        let x = p.x().to_f64().unwrap();
+        let y = p.y().to_f64().unwrap();
+        Vertex::Point(Point2::new(
+            T::from_f64(x).unwrap(),
+            T::from_f64(y).unwrap(),
+        ))
+    }
+
+    pub fn intersection<P: geo::CoordNum>(p: geo::Point<P>) -> Self {
+        let x = p.x().to_f64().unwrap();
+        let y = p.y().to_f64().unwrap();
+        Vertex::Intersection(Point2::new(
+            T::from_f64(x).unwrap(),
+            T::from_f64(y).unwrap(),
+        ))
+    }
+
+    pub fn inner(&self) -> &Point2<T> {
+        match self {
+            Vertex::Point(p) => p,
+            Vertex::Intersection(p) => p,
+        }
+    }
+}
+
+impl<'a, T> Offset<'a, T> for NurbsCurve2D<T>
+where
+    T: FloatingPoint,
+{
+    type Output = anyhow::Result<CompoundCurve2D<T>>;
+    type Option = CurveOffsetOption<T>;
+
+    /// Offset the NURBS curve by a given distance with a given epsilon
+    fn offset(&'a self, option: Self::Option) -> Self::Output {
+        let CurveOffsetOption {
+            distance,
+            normal_tolerance: tol,
+            corner_type,
+        } = option;
+
+        let tess = tessellate_nurbs_curve(self, tol);
+        let offset = tess
+            .into_iter()
+            .map(|(p, tangent)| {
+                let normal = Vector2::new(tangent.y, -tangent.x);
+                p + normal * distance
+            })
+            .collect_vec();
+
+        // find intersections
+
+        if self.degree() == 1 {
+            let lines = offset
+                .chunks(2)
+                .map(|w| {
+                    let p0 = &w[0];
+                    let p1 = &w[1];
+                    to_line_helper(p0, p1)
+                })
+                .collect_vec();
+
+            let its = lines
+                .windows(2)
+                .map(|w| {
+                    let l0 = &w[0];
+                    let l1 = &w[1];
+                    geo::algorithm::line_intersection::line_intersection(l0.clone(), l1.clone())
+                })
+                .collect_vec();
+
+            let n = lines.len();
+            let vertices: Vec<Vertex<T>> = lines
+                .into_iter()
+                .enumerate()
+                .flat_map(|(i, line)| {
+                    if i == 0 {
+                        // head
+                        match &its[0] {
+                            Some(LineIntersection::SinglePoint {
+                                intersection,
+                                is_proper: _,
+                            }) => {
+                                vec![
+                                    line.start_point().into(),
+                                    Vertex::intersection((*intersection).into()),
+                                ]
+                            }
+                            _ => {
+                                vec![line.start_point().into(), line.end_point().into()]
+                            }
+                        }
+                    } else if i == n - 1 {
+                        // tail
+                        let prev = &its[i - 1];
+                        match prev {
+                            Some(_) => {
+                                vec![line.end_point().into()]
+                            }
+                            _ => {
+                                vec![line.start_point().into(), line.end_point().into()]
+                            }
+                        }
+                    } else {
+                        // middle
+                        let prev = &its[i - 1];
+                        let cur = &its[i];
+                        let start = match prev {
+                            Some(LineIntersection::SinglePoint {
+                                intersection: _,
+                                is_proper: _,
+                            }) => None,
+                            _ => Some(line.start_point().into()),
+                        };
+
+                        let end = match cur {
+                            Some(LineIntersection::SinglePoint {
+                                intersection,
+                                is_proper: _,
+                            }) => Vertex::intersection((*intersection).into()),
+                            _ => line.end_point().into(),
+                        };
+
+                        vec![start, Some(end)].into_iter().flatten().collect_vec()
+                    }
+                })
+                .collect_vec();
+
+            return match corner_type {
+                CurveOffsetCornerType::None => Ok(Self::polyline(&offset, false).into()),
+                CurveOffsetCornerType::Sharp => {
+                    // scan to connect vertices
+                    let n = vertices.len();
+                    let mut cursor = 0;
+                    let mut scanned: Vec<Point2<T>> = vec![vertices[0].clone().into()];
+
+                    let delta = distance.abs() * T::from_f64(2.0).unwrap();
+
+                    while cursor < n {
+                        if cursor + 3 >= n {
+                            let rest: Vec<Point2<T>> = vertices[cursor..]
+                                .iter()
+                                .map(|v| v.clone().into())
+                                .collect_vec();
+                            scanned.extend(rest);
+                            break;
+                        }
+
+                        let v1 = &vertices[cursor + 1];
+                        if let Vertex::Intersection(it) = v1 {
+                            scanned.push(it.clone());
+                            cursor += 1;
+                            continue;
+                        }
+
+                        // find the intersection of the line v0-v1 & v2-v3
+                        let v0 = &vertices[cursor];
+                        let d0 = (v1.inner() - v0.inner()).normalize() * delta;
+                        let l0 = to_line_helper(v0.inner(), &(v1.inner() + d0));
+
+                        let v2 = &vertices[cursor + 2];
+                        let v3 = &vertices[cursor + 3];
+                        let d1 = (v3.inner() - v2.inner()).normalize() * delta;
+                        let l1 = to_line_helper(&(v2.inner() - d1), v3.inner());
+
+                        let it = geo::algorithm::line_intersection::line_intersection(l0, l1);
+                        let it = it
+                            .and_then(|it| match it {
+                                LineIntersection::SinglePoint {
+                                    intersection: p,
+                                    is_proper: _,
+                                } => Some(p),
+                                _ => None,
+                            })
+                            .ok_or(anyhow::anyhow!("no intersection"))?;
+                        scanned.push(Point2::new(
+                            T::from_f64(it.x).unwrap(),
+                            T::from_f64(it.y).unwrap(),
+                        ));
+                        cursor += 2;
+                    }
+                    Ok(Self::polyline(&scanned, false).into())
+                }
+                CurveOffsetCornerType::Round => todo!(),
+                CurveOffsetCornerType::Smooth => todo!(),
+                CurveOffsetCornerType::Chamfer => Self::try_interpolate(
+                    &vertices.into_iter().map(|v| v.into()).collect_vec(),
+                    self.degree(),
+                )
+                .map(|c| c.into()),
+            };
+        } else {
+        };
+
+        todo!()
+    }
+}
+
+impl<'a, T> Offset<'a, T> for NurbsCurve3D<T>
+where
+    T: FloatingPoint,
+{
+    type Output = Self;
+    type Option = CurveOffsetOption<T>;
+
+    /// Offset the NURBS curve by a given distance with a given epsilon
+    fn offset(&'a self, option: Self::Option) -> Self::Output {
+        let CurveOffsetOption {
+            distance,
+            normal_tolerance: epsilon,
+            corner_type,
+        } = option;
+
+        let tess = tessellate_nurbs_curve(self, epsilon);
+        let offset = tess
+            .into_iter()
+            .map(|(p, tangent)| {
+                let normal = tangent;
+                p + normal * distance
+            })
+            .collect_vec();
+
+        todo!()
+    }
+}
+
+fn tessellate_nurbs_curve<T: FloatingPoint, D: DimName>(
+    curve: &NurbsCurve<T, D>,
+    normal_tolerance: T,
+) -> Vec<(
+    OPoint<T, DimNameDiff<D, U1>>,
+    OVector<T, DimNameDiff<D, U1>>,
+)>
+where
+    D: DimNameSub<U1>,
+    DefaultAllocator: Allocator<D>,
+    DefaultAllocator: Allocator<DimNameDiff<D, U1>>,
+{
+    if curve.degree() == 1 {
+        let pts = curve.dehomogenized_control_points();
+        pts.windows(2)
+            .map(|w| {
+                let p0 = &w[0];
+                let p1 = &w[1];
+                let tangent = p1 - p0;
+                [(p0.clone(), tangent.clone()), (p1.clone(), tangent)]
+            })
+            .flatten()
+            .collect()
+    } else {
+        let mut rng = rand::rng();
+        let (start, end) = curve.knots_domain();
+        tessellate_curve_adaptive(curve, start, end, normal_tolerance, &mut rng, &|t, p| {
+            (p, curve.tangent_at(t))
+        })
+    }
+}
+
+/// Convert a line segment to a geo::Line
+fn to_line_helper<T: FloatingPoint>(p0: &Point2<T>, p1: &Point2<T>) -> geo::Line {
+    let x0 = <f64 as NumCast>::from(p0.x).unwrap();
+    let y0 = <f64 as NumCast>::from(p0.y).unwrap();
+    let x1 = <f64 as NumCast>::from(p1.x).unwrap();
+    let y1 = <f64 as NumCast>::from(p1.y).unwrap();
+    geo::Line::new(geo::coord! { x: x0, y: y0 }, geo::coord! { x: x1, y: y1 })
+}
+
+#[cfg(test)]
+mod tests {
+    use nalgebra::Point2;
+
+    use crate::curve::NurbsCurve2D;
+
+    use super::*;
+
+    #[test]
+    fn offset_nurbs_curve() {
+        let points = vec![
+            Point2::new(0.0, 0.0),
+            Point2::new(1.0, 0.0),
+            Point2::new(1.0, 1.0),
+            Point2::new(0.0, 1.0),
+        ];
+        let polyline = NurbsCurve2D::polyline(&points, false);
+        let knots = polyline.knots().to_vec();
+        let n = knots.len();
+        let knots = knots[1..n - 1].to_vec(); // remove the first and last knot
+    }
+}
