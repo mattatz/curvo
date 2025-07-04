691557c12460c29ca8b44c4c4a10105698c223fa
Ryan <132001783+TimTheBig@users.noreply.github.com>
2025-07-04 17:57:43 -0400

    Remove `Boolean` methods `union`, `intersection`, and `difference` default bodys

--- src/boolean/mod.rs
+++ src/boolean/mod.rs
@@ -17,17 +17,11 @@ pub trait Boolean<T> {
     type Output;
     type Option;
 
-    fn union(&self, other: T, option: Self::Option) -> Self::Output {
-        self.boolean(BooleanOperation::Union, other, option)
-    }
+    fn union(&self, other: T, option: Self::Option) -> Self::Output;
 
-    fn intersection(&self, other: T, option: Self::Option) -> Self::Output {
-        self.boolean(BooleanOperation::Intersection, other, option)
-    }
+    fn intersection(&self, other: T, option: Self::Option) -> Self::Output;
 
-    fn difference(&self, other: T, option: Self::Option) -> Self::Output {
-        self.boolean(BooleanOperation::Difference, other, option)
-    }
+    fn difference(&self, other: T, option: Self::Option) -> Self::Output;
 
     fn boolean(&self, operation: BooleanOperation, other: T, option: Self::Option) -> Self::Output {
         match operation {
