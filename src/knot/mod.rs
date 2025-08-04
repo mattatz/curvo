c584c429f1a052f9e40aad064fdc8f4c8b979644
mattatz <masatatsu.nakamura@gmail.com>
2025-08-04 16:43:19 +0900

    Add KnotSide enum and knot span search with hint

--- src/knot/mod.rs
+++ src/knot/mod.rs
@@ -2,3 +2,17 @@ pub mod knot_multiplicity;
 pub mod knot_vector;
 pub use knot_multiplicity::*;
 pub use knot_vector::*;
+
+/// Side of a knot
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum KnotSide {
+    None,
+    Below,
+    Above,
+}
+
+impl Default for KnotSide {
+    fn default() -> Self {
+        KnotSide::None
+    }
+}
