3db0dadc7f2e07d1fb91e1dcd2abb9dfe3cc01a9
mattatz <masatatsu.nakamura@gmail.com>
2025-08-19 15:35:30 +0900

    Add surface-plane intersection module structure

--- src/intersects/surface_plane/mod.rs
+++ src/intersects/surface_plane/mod.rs
@@ -1,1 +1,7 @@
+pub mod intersection_surface_plane;
+pub mod surface_plane_intersection_bfgs;
+pub mod surface_plane_intersection_problem;
 
+pub use intersection_surface_plane::*;
+pub use surface_plane_intersection_bfgs::*;
+pub use surface_plane_intersection_problem::*;
