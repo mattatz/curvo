1ef4207b48e8d1e3edd74e6ef8f387aac34f4538
mattatz <masatatsu.nakamura@gmail.com>
2025-08-25 12:24:06 +0900

    Refactor surface-plane intersection logic

--- src/intersects/surface_plane/mod.rs
+++ src/intersects/surface_plane/mod.rs
@@ -4,7 +4,7 @@ pub mod surface_plane_intersection_problem;
 
 use argmin::core::{ArgminFloat, Executor, State};
 pub use intersection_surface_plane::*;
-use nalgebra::{Matrix2, Point3, Vector2};
+use nalgebra::{Const, Matrix2, Point3, Vector2};
 pub use surface_plane_intersection_bfgs::*;
 pub use surface_plane_intersection_problem::*;
 
@@ -14,6 +14,28 @@ use crate::{
     surface::{NurbsSurface3D, UVDirection},
 };
 
+/// Find intersection leaf nodes between a surface and a plane
+pub fn find_surface_plane_intersection_leaf_nodes<'a, T: FloatingPoint + ArgminFloat>(
+    surface: &'a NurbsSurface3D<T>,
+    plane: &'a Plane<T>,
+    knot_domain_division: usize,
+) -> anyhow::Result<Vec<SurfaceBoundingBoxTree<'a, T, Const<4>>>> {
+    // Create bounding box tree for the surface
+    let tree = SurfaceBoundingBoxTree::new(
+        surface,
+        UVDirection::U,
+        Some({
+            let (u_interval, v_interval) = surface.knots_domain_interval();
+            let div = T::from_usize(knot_domain_division).unwrap();
+            (u_interval / div, v_interval / div)
+        }),
+    );
+
+    // Check each segment of the surface against the plane
+    let leaf_nodes = tree.traverse_leaf_nodes_with_plane(plane);
+    Ok(leaf_nodes)
+}
+
 /// Find intersection points between a surface and a plane
 pub fn find_surface_plane_intersection_points<T: FloatingPoint + ArgminFloat>(
     surface: &NurbsSurface3D<T>,
@@ -22,19 +44,10 @@ pub fn find_surface_plane_intersection_points<T: FloatingPoint + ArgminFloat>(
 ) -> anyhow::Result<Vec<(Point3<T>, (T, T))>> {
     let options = options.unwrap_or_default();
 
-    // Create bounding box tree for the surface
-    let tree = SurfaceBoundingBoxTree::new(
-        surface,
-        UVDirection::U,
-        Some({
-            let (u_interval, v_interval) = surface.knots_domain_interval();
-            let div = T::from_usize(options.knot_domain_division).unwrap();
-            (u_interval / div, v_interval / div)
-        }),
-    );
+    let leaf_nodes =
+        find_surface_plane_intersection_leaf_nodes(surface, plane, options.knot_domain_division)?;
 
-    // Check each segment of the surface against the plane
-    let leaf_nodes = tree.traverse_leaf_nodes_with_plane(plane);
+    // Create bounding box tree for the surface
     let (u_domain, v_domain) = surface.knots_domain();
 
     // Collect intersection points from all leaf nodes
